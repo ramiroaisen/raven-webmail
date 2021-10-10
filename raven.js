26f40ac15b7e0e09e2e5079c959f3aef2fc92fd9
ramiroaisen <ramiroaisen@gmail.com>
2021-10-10 16:16:14 -0300

    init v1

--- raven.js
+++ raven.js
@@ -1,3 +1,4 @@
-require("source-map-support").install();
+#!/usr/bin/env node
 global.__RAVEN_IMPORT_SVELTEKIT__ = () => import("./app/build/middlewares.js");
+require("source-map-support").install();
 require("./server/dist/cli.js");
