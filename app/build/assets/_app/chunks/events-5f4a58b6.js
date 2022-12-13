3022626f544dbe929c942bd2eb8832a9fd340134
Ramiro <ramiroaisen@gmail.com>
2022-12-13 11:08:20 -0300

    clean build files

--- app/build/assets/_app/chunks/events-5f4a58b6.js
+++
@@ -1,1 +0,0 @@
-const t=()=>{let t=0;const e=new Map;return{on:s=>{const n=++t;return e.set(n,s),()=>{e.delete(n)}},dispatch:t=>{for(const s of e.values())s(t)}}},e=t(),s=t(),n=t();export{e as C,s as E,n as a};
--- server/dist/metadata.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RAVEN_SIGNATURE_META_KEY = void 0;
-exports.RAVEN_SIGNATURE_META_KEY = "ravenSignatureHTML";
-//# sourceMappingURL=metadata.js.map
