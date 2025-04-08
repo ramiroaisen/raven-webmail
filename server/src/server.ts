92cee7e01c3e035420d46f205eb0f64aec14f1da
ramiroaisen <52116153+ramiroaisen@users.noreply.github.com>
2025-04-08 17:00:42 -0300

    r2.ready

--- server/src/server.ts
+++ server/src/server.ts
@@ -56,6 +56,6 @@ export const start = async (config: Config) => {
   const server = createServer(config, app);
   server.listen(config.port, () => {;
     console.log(`> ${chalk.yellow(config.ssl ? "https" : "http")} server listening at port ${chalk.yellow(config.port)}`);
-    console.log("#r2.READY");
+    console.warn("#r2.READY");
   })
 }
